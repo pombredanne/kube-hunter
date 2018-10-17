caf3b534b36f55834266e11392f26158d6a28caf
oriagmon <oriagmon1@gmail.com>
2018-10-17 12:45:21 +0300

    TODO BRANCH- Handle that exception

--- src/modules/discovery/apiserver.py
+++ src/modules/discovery/apiserver.py
@@ -5,6 +5,7 @@ from ...core.types import Hunter
 from ...core.events import handler
 from ...core.events.types import OpenPortEvent, Service, Event
 
+
 class ReadOnlyKubeletEvent(Service, Event):
     """The read-only port on the kubelet serves health probing endpoints, and is relied upon by many kubernetes componenets"""
     def __init__(self):
@@ -16,6 +17,7 @@ class ApiServer(Service, Event):
     def __init__(self):
         Service.__init__(self, name="API Server")
 
+
 @handler.subscribe(OpenPortEvent, predicate=lambda x: x.port==443 or x.port==6443)
 class ApiServerDiscovery(Hunter):
     """Api Server Discovery
@@ -26,7 +28,11 @@ class ApiServerDiscovery(Hunter):
 
     def execute(self):
         logging.debug("Attempting to discover an Api server")
-        main_request = requests.get("https://{}:{}".format(self.event.host, self.event.port), verify=False).text
-        if "code" in main_request:
-            self.event.role = "Master"
-        self.publish_event(ApiServer())
+        try:
+            main_request = requests.get("https://{}:{}".format(self.event.host, self.event.port), verify=False).text
+            if "code" in main_request:
+                self.event.role = "Master"
+        except requests.exceptions.SSLError:#TODO: Understand how to Handle that exception
+            pass
+        finally:
+            self.publish_event(ApiServer())
